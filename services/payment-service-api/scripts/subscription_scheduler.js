b65f52700a9adf76a4255a34ac6f5471ea9ae0dd
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2020-07-29 17:56:54 -0300

    # This is a combination of 5 commits. # This is the 1st commit message:
    parent b3494da6fd3361f953c2ce8d635ad6979ceb9388
    author Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com> 1596056214 -0300
    committer Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com> 1600785613 -0300
    
    ✨validação da conta bancária durante a requisição do saque
    
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
    
    # This is the commit message #2:
    
    add unique index on gateway_payables(gateway_id)
    
    # This is the commit message #3:
    
    check if reward is present before refresh metric
    
    # This is the commit message #4:
    
    Fix subscription missing payables routine
    
    # This is the commit message #5:
    
    Fix import missing payables rake task

--- services/payment-service-api/scripts/subscription_scheduler.js
+++ services/payment-service-api/scripts/subscription_scheduler.js
@@ -2,9 +2,7 @@
 'use strict';
 
 const { Pool } = require('pg');
-const pagarme = require('pagarme');
 const { handleError } = require('../lib/error_handling');
-const { importMissingPayables } = require('./import_missing_payables')
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -161,14 +159,22 @@ const recursive_calls = () => {
         }, (process.env.SET_INTERVAL || 60000));
     };
 
+<<<<<<< HEAD
+=======
     const missingPayables = () => {
         setTimeout(async () => {
-            console.log('Importing missing payables')
-            const client = await pool.connect();
-            importMissingPayables(client)
-        }, 3600000) // 1 HOUR
+            try {
+                console.log('Importing missing payables')
+                const client = await pool.connect();
+                importMissingPayables(client).then(() => { missingPayables() })
+            } catch (e) {
+                handleError(e);
+                console.log(e);
+            }
+        }, 300000) // 5 minutes
     }
 
+>>>>>>> 172b6f533... Fix subscription missing payables routine
     rec_charge();
     rec_errors_charge();
     refuse_expired();
@@ -176,7 +182,6 @@ const recursive_calls = () => {
     inactive_invalid_subs();
     notify_expiring();
     recharge_or_inactive_card_subscriptions();
-    missingPayables();
 };
 
 recursive_calls();
